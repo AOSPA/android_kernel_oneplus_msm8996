5c6680dca0276b949436db210f6daa636232adba
Alex Naidis <alex.naidis@linux.com>
2017-08-14 16:33:39 -0400

    Revert "BACKPORT: PM / sleep: Go direct_complete if driver has no callbacks"
    This reverts commit 16d06c4caa61fd961b196f6f1df635c9f9522c40.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/base/power/power.h
+++ drivers/base/power/power.h
@@ -51,7 +51,6 @@ extern void device_pm_remove(struct device *);
 extern void device_pm_move_before(struct device *, struct device *);
 extern void device_pm_move_after(struct device *, struct device *);
 extern void device_pm_move_last(struct device *);
-extern void device_pm_check_callbacks(struct device *dev);
 
 #else /* !CONFIG_PM_SLEEP */
 
@@ -69,8 +68,6 @@ static inline void device_pm_move_before(struct device *deva,
 static inline void device_pm_move_after(struct device *deva,
 					struct device *devb) {}
 static inline void device_pm_move_last(struct device *dev) {}
-
-static inline void device_pm_check_callbacks(struct device *dev) {}
 
 #endif /* !CONFIG_PM_SLEEP */
 
